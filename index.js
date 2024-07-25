c2dc91d743816222c9e5531f6ac92a1763ca728d
dotuser <ahmedhassaan@tutanota.com>
2024-07-25 15:29:22 +0500

    Messenger API Webhook

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+
+const messengerToken = process.env.MESSENGER_TOKEN
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.post('/webhook', (req, res) => {
+  const { hubMode, hubVerifyToken, hubChallenge } = req.query;
+
+  if (hubMode === 'subscribe' && hubVerifyToken === messengerToken) {
+    res.send(hubChallenge);
+  } else {
+    res.status(400).send('Invalid request');
+  }
 });
 
 app.listen(PORT, () => {
