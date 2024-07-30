45f239dd81fe0d6ea4f17e6db091acba5fe71ab3
dotuser <ahmedhassaan@tutanota.com>
2024-07-30 16:02:12 +0500

    FB Page Res Test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const axios = require('axios');
+const request = require('request');
+// const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
@@ -8,7 +9,7 @@ const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   console.log('Server Running');
-  res.send('Home');
+  res.send('Restaurant Chat Bot...');
 });
 
 app.get('/webhook', (req, res) => {
@@ -59,15 +60,54 @@ app.post('/webhook', (req, res) => {
 // Add this function to send a response
 function sendResponse(recipientId, message) {
   console.log('Send Messenger API Res func.. called!');
-  axios.post('https://graph.facebook.com/v14.0/me/messages', {
-    recipient: { id: recipientId },
-    message: { text: message },
-  }, {
-    headers: {
-      'Authorization': `Bearer ${process.env.MESSENGER_ACCESS_TOKEN}`,
-      'Content-Type': 'application/json',
+
+  let response;
+
+  // Check if the message contains text
+  if (message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": "Server's LIVE..."
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(recipientId, message);    
+
+  // axios.post('https://graph.facebook.com/v14.0/me/messages', {
+  //   recipient: { id: recipientId },
+  //   message: { text: message },
+  // }, {
+  //   headers: {
+  //     'Authorization': `Bearer ${process.env.MESSENGER_ACCESS_TOKEN}`,
+  //     'Content-Type': 'application/json',
+  //   },
+  // });
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
     },
-  });
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.FB_PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
 }
 
 app.listen(PORT, () => {
